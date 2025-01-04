52d8b7dc3813d92d50e3422123d62bf2d0dda6c5
Varun <varun.g.dsu@gmail.com>
2025-01-04 12:49:04 +0530

    implemented cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,16 @@ import authRoutes from '../src/routes/auth.route.js';
 import messageRoutes from '../src/routes/message.route.js';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'
+import cors from 'cors';
 import globalErrorHandler from './middleware/globalErrorHandler.js';
 import { app, server } from './socket/socket.js';
 
 dotenv.config();
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
