f0c637a578878da614a6d3b9877c2c1fdc8b2bba
Varun <varun.g.dsu@gmail.com>
2025-01-04 12:50:39 +0530

    Implemented message delivery status

--- frontend/src/components/messages/Message.tsx
+++ frontend/src/components/messages/Message.tsx
@@ -3,26 +3,45 @@ import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }: { message: MessageType }) => {
-	const {authUser} = useAuthContext();
-	const {selectedConversation} = useConversation();
+    const { authUser } = useAuthContext();
+    const { selectedConversation } = useConversation();
 
-	const fromMe = message?.senderId === authUser?.id;
-	const img = fromMe
-	? authUser?.profileImage
-	: selectedConversation?.profileImage;
-	const chatClass = fromMe ? "chat-end" : "chat-start";
+    const fromMe = message?.senderId === authUser?.id;
+    const img = fromMe
+        ? authUser?.profileImage
+        : selectedConversation?.profileImage;
+    const chatClass = fromMe ? "chat-end" : "chat-start";
 
-	const bubbleBg = fromMe ? "bg-blue-500" : "";
-	return (
-		<div className={`chat ${chatClass}`}>
-			<div className='hidden md:block chat-image avatar'>
-				<div className='w-6 md:w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={img} />
-				</div>
-			</div>
-			<p className={`chat-bubble text-white ${bubbleBg} text-sm md:text-md`}>{message.body}</p>
-			<span className='chat-footer opacity-50 text-xs flex gap-1 items-center text-white'>{extractTime(message.createdAt)}</span>
-		</div>
-	);
+    const bubbleBg = fromMe ? "bg-blue-500" : "";
+    const shakeClass = message.shouldShake ? "shake" : "";
+
+    // Status indicators
+    const status = fromMe
+        ? message.read
+            ? "Read"
+            : message.delivered
+            ? "Delivered"
+            : "Sending..."
+        : "";
+
+    return (
+        <div className={`chat ${chatClass}`}>
+            <div className="hidden md:block chat-image avatar">
+                <div className="w-6 md:w-10 rounded-full">
+                    <img alt="Tailwind CSS chat bubble component" src={img} />
+                </div>
+            </div>
+            <p
+                className={`chat-bubble text-white ${bubbleBg} ${shakeClass} text-sm md:text-md`}
+            >
+                {message.body}
+            </p>
+            <span className="chat-footer opacity-50 text-xs flex gap-1 items-center text-white">
+                {extractTime(message.createdAt)}{" "}
+                {fromMe && <span>{status}</span>}
+            </span>
+        </div>
+    );
 };
+
 export default Message;
--- frontend/src/components/sidebar/Conversation.tsx
+++ frontend/src/components/sidebar/Conversation.tsx
@@ -1,49 +1,60 @@
+import { useState } from "react";
 import { useAuthContext } from "../../context/AuthContext";
 import { useSocketContext } from "../../context/SocketContext";
 import { ConversationType } from "../../types/global";
 import useConversation from "../../zustand/useConversation";
 
-const Conversation = ({ conversation, emoji }: { conversation: ConversationType, emoji: string }) => {
-	const { setSelectedConversation, selectedConversation } = useConversation();
+const Conversation = ({ conversation }: { conversation: ConversationType }) => {
+    const {
+        setSelectedConversation,
+        resetUnreadMessages,
+        selectedConversation,
+    } = useConversation();
     const { authUser } = useAuthContext();
     const isMe = conversation.id === authUser?.id;
 	const isSelected = selectedConversation?.id === conversation.id;
-
     const { onlineUsers} = useSocketContext();
+    console.log("online users: " + onlineUsers);
 	const isOnline = onlineUsers.includes(conversation.id);
 
-    if (isMe) {
-        return null;
-    }
-	return (
-        <>
-            <div
-                className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected ? "bg-sky-500" : ""}`}
-                onClick={() => setSelectedConversation(conversation)}
-            >
-                <div className={`avatar ${isOnline ? "online" : ""}`}>
-                    <div className="w-8 md:w-12 rounded-full">
-                        <img
-                            src={conversation.profileImage}
-                            alt="user avatar"
-                        />
-                    </div>
-                </div>
+    console.log("is online: " + isOnline);
 
-                <div className="flex flex-col flex-1">
-                    <div className="flex gap-3 justify-between">
-                        <p className="font-bold text-gray-200 text-sm md:text-md">
-                            {conversation.fullName}
-                        </p>
-                        <span className="text-xl hidden md:inline-block">
-                            {emoji}
-                        </span>
-                    </div>
-                </div>
+    const handleSelectConversation = () => {
+        setSelectedConversation(conversation);
+        resetUnreadMessages(conversation.id); // Reset unread count when the conversation is opened
+    };
+
+    if (isMe) return null;
+
+    return (
+        <div
+            className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${
+                isSelected ? "bg-sky-500" : ""
+            }`}
+            onClick={handleSelectConversation}
+        >
+            {/* UI for unread count */}
+            <div className={`flex gap-2 ${isOnline ? "online" : ""}`}> 
+                <img 
+                    src={conversation.profileImage}
+                    alt={conversation.fullName}
+                    className="w-10 h-10 rounded-full"
+                />
+                <p className="font-bold">{conversation.fullName}</p>
+
+                <span className="text-xs text-gray-500">
+                    {conversation.unreadCount > 0
+                        ? (
+                            <span className="bg-red-500 text-white rounded-full px-2 py-1">
+                                {conversation.unreadCount}
+                            </span>
+                        )
+                        : ""}
+                </span>
             </div>
-
-            <div className="divider my-0 py-0 h-1" />
-        </>
+        </div>
     );
 };
+
 export default Conversation;
+
